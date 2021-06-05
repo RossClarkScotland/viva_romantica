add959968e4f0fca49026ec9819c2f9677fc557c
RossClarkScotland <rossclarkscotland@gmail.com>
2021-06-05 16:34:20 +0000

    add course cards

--- courses/views.py
+++ courses/views.py
@@ -6,9 +6,11 @@ def all_courses(request):
     """shows all courses"""
 
     courses = Course.objects.all()
+    spanish_courses = Course.objects.filter(name__contains='Spanish')
 
     context = {
         'courses': courses,
+        'spanish_courses': spanish_courses,
     }
 
     return render(request, 'courses/courses.html', context)
