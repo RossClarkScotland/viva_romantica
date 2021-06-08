4a39eaabab2f6aa229f82cedc5d828cecdc400a3
RossClarkScotland <rossclarkscotland@gmail.com>
2021-06-08 21:50:37 +0000

    add functionality to add to shoppong bag form

--- shopping_bag/views.py
+++ shopping_bag/views.py
@@ -1,7 +1,17 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 # Create your views here.
 def view_shopping_bag(request):
     """renders shopping bag page"""
 
     return render(request, 'shopping_bag/shopping_bag.html')
+
+
+def add_to_shopping_bag(request, item_id):
+    """ Add a course to the shopping bag """
+    redirect_url = request.POST.get('redirect_url')
+    shopping_bag = request.session.get('shopping_bag', {})
+
+    request.session['shopping_bag'] = shopping_bag
+    print(request.session['shopping_bag'])
+    return redirect(redirect_url)
