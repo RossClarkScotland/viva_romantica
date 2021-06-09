34bc7b0020cfef8341a751166ca5bf2bc5047201
RossClarkScotland <rossclarkscotland@gmail.com>
2021-06-09 19:09:43 +0000

    hook up context processor to shopping bag

--- shopping_bag/contexts.py
+++ shopping_bag/contexts.py
@@ -1,4 +1,6 @@
 from django.conf import settings
+from django.shortcuts import get_object_or_404
+from courses.models import Course
 
 def shopping_bag_contents(request):
 
@@ -6,6 +8,17 @@ def shopping_bag_contents(request):
     total = 0
     grand_total = 0
     product_count = 0
+    shopping_bag = request.session.get('shopping_bag', {})
+
+    for item_id, quantity in shopping_bag.items():
+        course = get_object_or_404(Course, pk=item_id)
+        total += quantity * course.price
+        product_count += quantity
+        shopping_bag_items.append({
+            'item_id': item_id,
+            'quantity': quantity,
+            'course': course,
+        })
 
     if total >= settings.COURSE_DISCOUNT_POINT:
         grand_total = total - 20
