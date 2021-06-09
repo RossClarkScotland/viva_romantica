ad18db6419896fe8eab19433832edac6430c82c7
RossClarkScotland <rossclarkscotland@gmail.com>
2021-06-09 18:21:23 +0000

    add env.py and secret key

--- shopping_bag/views.py
+++ shopping_bag/views.py
@@ -8,9 +8,19 @@ def view_shopping_bag(request):
 
 
 def add_to_shopping_bag(request, item_id):
-    """ Add a course to the shopping bag """
+    """ Add courses to the shopping bag """
+
+    """Logic as shown in the Boutique Ado walkthrough project"""
+
+    quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     shopping_bag = request.session.get('shopping_bag', {})
+
+    if item_id in list(shopping_bag.keys()):
+        shopping_bag[item_id] += quantity
+    else:
+        shopping_bag[item_id] = quantity
+
 
     request.session['shopping_bag'] = shopping_bag
     print(request.session['shopping_bag'])
--- viva_romantica/settings.py
+++ viva_romantica/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-nbs9zl@(6*az)q8x+jf=ggr1kp@cxtk!jhdrfq-9uu-!=c$(2#'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
