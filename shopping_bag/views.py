a24ad40abf8dde8377cdcef45f0f2b57cbc0da83
RossClarkScotland <rossclarkscotland@gmail.com>
2021-06-13 08:54:28 +0000

    add toast msgs for removing bag items and changing qty

--- shopping_bag/views.py
+++ shopping_bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
 from django.contrib import messages
 from courses.models import Course
 
@@ -12,13 +12,14 @@ def view_shopping_bag(request):
 def add_to_shopping_bag(request, item_id):
     """ Add courses to the shopping bag """
 
-    course = Course.objects.get(pk=item_id)
+    course = get_object_or_404(Course, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     shopping_bag = request.session.get('shopping_bag', {})
 
     if item_id in list(shopping_bag.keys()):
         shopping_bag[item_id] += quantity
+        messages.success(request, f'You updated the quantity of {course.name} in your shopping bag!')
     else:
         shopping_bag[item_id] = quantity
         messages.success(request, f'You added {course.name} to your bag!')
@@ -32,13 +33,16 @@ def add_to_shopping_bag(request, item_id):
 def change_shopping_bag(request, item_id):
     """ changes number of courses in the shopping bag """
 
+    course = get_object_or_404(Course, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     shopping_bag = request.session.get('shopping_bag', {})
 
     if quantity > 0:
         shopping_bag[item_id] = quantity
+        messages.success(request, f'You updated the quantity of {course.name} in your shopping bag!')
     else:
         shopping_bag.pop(item_id)
+        messages.success(request, f'You removed {course.name} from your shopping bag!')
 
     request.session['shopping_bag'] = shopping_bag
     return redirect(reverse(view_shopping_bag))
@@ -48,11 +52,14 @@ def remove_from_shopping_bag(request, item_id):
     """removes items from shopping bag"""
 
     try:
+        course = get_object_or_404(Course, pk=item_id)
         shopping_bag = request.session.get('shopping_bag', {})
         shopping_bag.pop(item_id)
+        messages.success(request, f'You removed {course.name} from your shopping bag!')
 
         request.session['shopping_bag'] = shopping_bag
         return HttpResponse(status=200)
 
     except Exception as e:
+        messages.error(request, f'There was an error in removing the item {e}')
         return HttpResponse(status=500)
