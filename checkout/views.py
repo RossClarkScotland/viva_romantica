6e3647cc31ed5c1edd6ac3ebf992af8c21001d51
RossClarkScotland <rossclarkscotland@gmail.com>
2021-06-16 17:58:52 +0000

    add basic stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -15,6 +15,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51J2jhOBsZwD5BzE8wyRJYa4tDBYik8umWwqV6KqmNOMde6HRryTqzVChPFK9ukAVWEYqmNCvB038vABPvAS1zIAN00xbOtfXxJ',
+        'client_secret': 'test',
     }
 
     return render(request, template, context)
